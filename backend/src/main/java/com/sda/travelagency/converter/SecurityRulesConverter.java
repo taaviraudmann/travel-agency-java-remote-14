32a0bb33da474361792b1ad82bc3a3f37143c5e5
taaviraudmann <taaviraudmann@gmail.com>
2022-08-21 14:05:23 +0300

    [change] - Converters created

---
+++ backend/src/main/java/com/sda/travelagency/converter/SecurityRulesConverter.java
@@ -0,0 +1,19 @@
+package com.sda.travelagency.converter;
+
+import com.sda.travelagency.dto.SecurityRulesDto;
+import com.sda.travelagency.entity.SecurityRules;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityRulesConverter implements Converter<SecurityRules, SecurityRulesDto> {
+
+    @Override
+    public SecurityRulesDto fromEntityToDto(SecurityRules entity) {
+        return null;
+    }
+
+    @Override
+    public SecurityRules fromDtoToEntity(SecurityRulesDto dto) {
+        return null;
+    }
+}
--- backend/src/main/java/com/sda/travelagency/converter/TripConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/TripConverter.java
@@ -1,15 +1,42 @@
 package com.sda.travelagency.converter;
 
 import com.sda.travelagency.dto.TripDto;
+import com.sda.travelagency.entity.HotelFacilities;
 import com.sda.travelagency.entity.Trip;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TripConverter implements Converter<Trip, TripDto> {
+    private final DestinationConverter destinationConverter;
+    private final SecurityRulesConverter securityRulesConverter;
+
+    private final HotelFacilitiesConverter hotelFacilitiesConverter ;
+
+    public TripConverter(DestinationConverter destinationConverter, SecurityRulesConverter securityRulesConverter, HotelFacilitiesConverter hotelFacilitiesConverter) {
+        this.destinationConverter = destinationConverter;
+        this.securityRulesConverter = securityRulesConverter;
+        this.hotelFacilitiesConverter = hotelFacilitiesConverter;
+    }
+
     @Override
     public TripDto fromEntityToDto(Trip trip) {
+        var destinationDto = destinationConverter.fromEntityToDto(trip.getDestination());
+        var securityRulesDto = securityRulesConverter.fromEntityToDto(trip.getSecurityRules());
+        var hotelFacilitiesDto = hotelFacilitiesConverter.fromEntityToDto(trip.getHotelFacilities());
 
-        return null;
+        return TripDto.builder()
+                .tripStartDate(trip.getTripStartDate())
+                .tripEndDate(trip.getTripEndDate())
+                .destination(destinationDto)
+                .cost(trip.getTripPrice().getCost().toString())
+                .cost(trip.getTripPrice().getCurrency())
+                .typeOfTransport(trip.getTypeOfTransport())
+                .paymentType(trip.getPaymentType())
+                .securityRules(securityRulesDto)
+                .mealTypes(trip.getMealTypes())
+                .hotelFacilities(hotelFacilitiesDto)
+                .photos(trip.getPhotos())
+                .build();
     }
 
     @Override

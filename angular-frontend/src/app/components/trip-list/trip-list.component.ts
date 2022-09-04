8af1fd4a480ffb275d1d85e9872dd2a8d24c6e31
taaviraudmann <taaviraudmann@gmail.com>
2022-09-04 13:35:15 +0300

    [change] - connection to backend in progress

--- angular-frontend/src/app/components/trip-list/trip-list.component.ts
+++ angular-frontend/src/app/components/trip-list/trip-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TripService} from "../../services/trip-service/trip-service.service";
 import {TripDto} from "../../models/trips";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-trip-list',
@@ -14,8 +15,12 @@ export class TripListComponent implements OnInit {
   constructor(private tripService: TripService) { }
 
   ngOnInit(): void {
+    console.log("trying to obtain data from server...")
     //place for query the backend and obtaining results
     this.tripService.getAllTrip()
+      .pipe(
+        tap(value => console.log("from server:" + value))// is for logging - we can see what's inside the stream of data
+      )
       .subscribe(value => this.trips = value)
   }
 

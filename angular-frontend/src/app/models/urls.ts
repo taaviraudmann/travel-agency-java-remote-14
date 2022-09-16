f8f11ee193f49b36b8c2aef3de7816d4b4068d8a
taaviraudmann <taaviraudmann@gmail.com>
2022-09-17 00:46:05 +0300

    [refactor] - fixing issue with fragile links - navigation component

--- angular-frontend/src/app/models/urls.ts
+++ angular-frontend/src/app/models/urls.ts
@@ -1,3 +1,8 @@
 const serverUrl = 'http://localhost:8080'
 export const allTripsUrl =`${serverUrl}/api/trips`
 
+//internal links
+export const homeUrl = 'home'
+export const defaultUrl =''
+export const aboutUsUrl = 'about-us'
+export const tripsUrl = 'all-trips'

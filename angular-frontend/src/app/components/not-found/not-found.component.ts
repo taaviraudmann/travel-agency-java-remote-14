9b350ab030550414963bffaf7b8332b744670b9c
taaviraudmann <taaviraudmann@gmail.com>
2022-09-17 01:23:56 +0300

    [add] - not found component created

---
+++ angular-frontend/src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import {homeUrl} from "../../models/urls";
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent  {
+
+  homeUrl = `/${homeUrl}`
+
+}

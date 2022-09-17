f5cda2df2e478d1837a8728ac89cd1565be3fecf
taaviraudmann <taaviraudmann@gmail.com>
2022-09-17 11:57:25 +0300

    [change] - JPA mappings for entities done

--- backend/src/main/java/com/sda/travelagency/dto/AttractionDto.java
+++ backend/src/main/java/com/sda/travelagency/dto/AttractionDto.java
@@ -5,6 +5,8 @@ import com.sda.travelagency.entity.enumeration.AgeRestrictions;
 import java.util.List;
 
 public record AttractionDto(
+
+        Long id,
         String description,
 
         AgeRestrictions ageRestrictions,
--- backend/src/main/java/com/sda/travelagency/entity/Attraction.java
+++ backend/src/main/java/com/sda/travelagency/entity/Attraction.java
@@ -3,16 +3,26 @@ package com.sda.travelagency.entity;
 import com.sda.travelagency.entity.enumeration.AgeRestrictions;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-
+@NoArgsConstructor
+@Entity
 public class Attraction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String description;
 
+    @Enumerated(EnumType.STRING)
     AgeRestrictions ageRestrictions;
 
+    @ElementCollection
+    @CollectionTable(name = "ATTRACTION_PHOTOS")
+    @Column(name = "PHOTO")
     List<String> photos;
 }
